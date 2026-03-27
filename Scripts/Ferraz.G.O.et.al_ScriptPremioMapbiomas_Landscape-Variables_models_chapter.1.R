5b3c26b21ab1d18de86e35d0aba8fd514e28798c
Gabriel O. Ferraz <106835195+GOFerraz@users.noreply.github.com>
2026-03-27 15:14:24 -0300

    Update Ferraz.G.O.et.al_ScriptPremioMapbiomas_Landscape-Variables_models_chapter.1.R

--- Scripts/Ferraz.G.O.et.al_ScriptPremioMapbiomas_Landscape-Variables_models_chapter.1.R
+++ Scripts/Ferraz.G.O.et.al_ScriptPremioMapbiomas_Landscape-Variables_models_chapter.1.R
@@ -1,5 +1,8 @@
 # ==============================================================================
-# Utiliza o modelo Hurdle para analisar PctWater (Logit + Gamma GAM).
+# Projeto: IMPACTO DOS CRUZAMENTOS DE ESTRADAS SOBRE A CONECTIVIDADE ECOHIDROLÓGICA DE IGARAPÉS AMAZÔNICOS EM PAISAGENS SOB DESMATAMENTO
+# Autor Principal/Contato Principal: Gabriel Oliveira Ferraz / gabriel.oferraz@usp.br
+
+# Autores: FERRAZ, G.O.; LEAL, C.G.; CORREA, K.A.B.; COUTO, T.B.D'A.; MAEOKA, L.T.Y.; BREJAO, G.L.; CARVALHO, D.R.; ALVES FILHO, M.A.; BARLOW, J.; FERRAZ, S.F.B.
 # ==============================================================================
 
 
